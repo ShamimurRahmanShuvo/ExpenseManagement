16c93712bd1eb3dcb7eeeff1475e9e3684ce363b
ShamimurRahmanShuvo <shamimur.shuvo@gmail.com>
2024-10-31 16:35:12 -0600

    Initial commit on expense management static pages

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+from datetime import date
+from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+## from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/add-expense")
+def add_expense():
+    return render_template("expense.html")
+
+
+@app.route("/add-income")
+def add_income():
+    return render_template("income.html")
+
+
+@app.route("/view-report")
+def view_report():
+    return render_template("report.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5008)
