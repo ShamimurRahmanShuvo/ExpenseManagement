9927577909189c76b69ea7816775bda7d5fbdd74
ShamimurRahmanShuvo <shamimur.shuvo@gmail.com>
2024-11-05 22:47:36 -0700

    Add DB expense and income along with their functionality

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
+from sqlalchemy import Integer, String, Float
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import RegisterForm, LoginForm, AddExpenseForm, ExpenseTypeForm, IncomeTypeForm, AddIncomeForm
@@ -39,18 +39,46 @@ class User(UserMixin, db.Model):
     username: Mapped[str] = mapped_column(String)
     email: Mapped[str] = mapped_column(String, unique=True)
     password: Mapped[str] = mapped_column(String)
+    expenses = relationship("Expense", back_populates="user")
+    incomes = relationship("Income", back_populates="user")
 
 
 class ExpenseType(db.Model):
     __tablename__ = "expense_type"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     expense_type: Mapped[str] = mapped_column(String)
+    expense_t = relationship("Expense", back_populates="expense_name")
+
+
+class Expense(db.Model):
+    __tablename__ = "expense"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    expense_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("expense_type.id"))
+    expense_name = relationship("ExpenseType", back_populates="expense_t")
+    user_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    user = relationship("User", back_populates="expenses")
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    expense_source: Mapped[str] = mapped_column(String(250), nullable=False)
+    amount: Mapped[float] = mapped_column(Float, nullable=False)
 
 
 class IncomeType(db.Model):
     __table_name__ = "income_type"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     income_type: Mapped[str] = mapped_column(String)
+    income_t = relationship("Income", back_populates="income_name")
+
+
+class Income(db.Model):
+    __tablename__ = "income"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    income_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("income_type.id"))
+    income_name = relationship("IncomeType", back_populates="income_t")
+    user_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
+    user = relationship("User", back_populates="incomes")
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    income_source: Mapped[str] = mapped_column(String(250), nullable=False)
+    amount: Mapped[float] = mapped_column(Float, nullable=False)
 
 
 with app.app_context():
@@ -159,7 +187,7 @@ def add_income_type():
             return redirect(url_for("add_income_type"))
 
         new_income_type = IncomeType(
-            income_type = income_name_capitalize
+            income_type=income_name_capitalize
         )
         db.session.add(new_income_type)
         db.session.commit()
@@ -169,7 +197,7 @@ def add_income_type():
     return render_template("income_type.html", form=form, current_user=current_user)
 
 
-@app.route("/add-income")
+@app.route("/add-income", methods=["GET", "POST"])
 def add_income():
     form = AddIncomeForm()
     results = db.session.execute(db.select(IncomeType)).scalars().all()
@@ -181,10 +209,28 @@ def add_income():
         choices.append((choice_id, choice_name))
 
     form.income_id.choices = choices
+
+    if form.validate_on_submit():
+        if not current_user.is_authenticated:
+            flash("You need to login to add your incomes")
+            return redirect(url_for("login"))
+
+        new_income = Income(
+            income_id=form.income_id.data,
+            user_id=current_user.id,
+            date=form.date.data,
+            income_source=form.source.data,
+            amount=form.amount.data
+        )
+        db.session.add(new_income)
+        db.session.commit()
+        flash("Income added successfully")
+        return redirect(url_for("add_income"))
+
     return render_template("income.html", form=form, current_user=current_user)
 
 
-@app.route("/add-expense")
+@app.route("/add-expense", methods=["GET", "POST"])
 def add_expense():
     form = AddExpenseForm()
     results = db.session.execute(db.select(ExpenseType)).scalars().all()
@@ -196,6 +242,24 @@ def add_expense():
         choices.append((choice_id, choice_name))
     # print(choices)
     form.expense_id.choices = choices
+
+    if form.validate_on_submit():
+        if not current_user.is_authenticated:
+            flash("You need to login to add your expenses")
+            return redirect(url_for("login"))
+
+        new_expense = Expense(
+            expense_id=form.expense_id.data,
+            user_id=current_user.id,
+            date=form.date.data,
+            expense_source=form.details.data,
+            amount=form.amount.data
+        )
+        db.session.add(new_expense)
+        db.session.commit()
+        flash("Expense added successfully")
+        return redirect(url_for("add_expense"))
+
     return render_template("expense.html", form=form, current_user=current_user)
 
 
