ce5813bb6807f180a8443495a5ed10c92716ed30
ShamimurRahmanShuvo <shamimur.shuvo@gmail.com>
2024-11-05 23:03:41 -0700

    Add key in .env file

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from forms import RegisterForm, LoginForm, AddExpenseForm, ExpenseTypeForm, Inco
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -27,7 +27,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///expense-sheet.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI", "sqlite:///expense-sheet.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
